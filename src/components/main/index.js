2072f1f4e306b8ca843383d0e7691c1b205ee2ae
highjoon <yundosa2@gmail.com>
2021-10-29 11:49:41 +0900

    (Create) Main Component

---
+++ src/components/main/index.js
@@ -0,0 +1,5 @@
+import HabitCard from './HabitCard';
+import Exp from './Exp';
+import Chracter from './Chracter';
+
+export { HabitCard, Exp, Chracter };
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -11,22 +11,30 @@ const GlobalStyle = createGlobalStyle`
     --color-login-bg: #27173f;
     --color-kakao: #fee500;
     --color-naver: #03c75a;
+    --color-progressbar: #f0f0f0;
+    --color-gauge: #7057fc;
+    --color-layout: #eeeeee;
 
     /* Font size */
     --font-large: 48px;
     --font-medium: 28px;
+    --font-semi-medium: 24px;
     --font-regular: 18px;
     --font-small: 16px;
     --font-micro: 14px;
+    --font-nano: 12px;
 
     /* Font weight */
     --weight-heavy-bold: 900;
     --weight-bold: 700;
     --weight-semi-bold: 600;
-    --weight-regular: 400;
+    --weight-regular: 500;
+    --weight-semi-regular: 400;
 
     /* Size */
     --size-border-radius: 6px;
+    --border-radius-progress: 10px;
+    --border-radius-checkBtn: 20px;
 
     /* Animation Duration */
     --animation-duration: 200ms;
