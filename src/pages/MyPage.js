e746560e2a0bc4d4a52cc89d10a7d9ffcf630bfd
Jaekyung Bae <jkbae09@gmail.com>
2021-10-29 19:50:59 +0900

    (Update) set selectAvatar page

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const MyPage = () => {
-  return <h1>My Page!</h1>;
+  return (
+    <>
+      <h1>my page</h1>
+      <ProfileImg src="" />
+    </>
+  );
 };
 
 export default MyPage;
+
+const ProfileImg = styled.img`
+  border-radius: 50%;
+  margin: 0 auto;
+`;
