983cf8224841cfda38ba0397d98545a983645af6
Jaekyung Bae <jkbae09@gmail.com>
2021-11-13 12:34:51 +0900

    (Refactor) update

--- src/components/myPage/UserInformation.js
+++ src/components/myPage/UserInformation.js
@@ -1,16 +1,24 @@
 import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useResetRecoilState } from 'recoil';
+import { useHistory } from 'react-router-dom';
 
+import { getCookie, deleteCookie } from '../../utils/cookie';
+
+import { authState } from '../../recoil/states/auth';
 import { myPageDataState } from '../../recoil/states/user';
 
 import UserInfoItem from './UserInfoItem';
 import { Modal } from '../../components/common';
 import { EditBox } from '../../components/myPage';
+import { BottomDialog } from '../dialog';
 
 const UserInformation = () => {
+  const resetAuth = useResetRecoilState(authState);
   const myPageData = useRecoilValue(myPageDataState); // 비동기요청
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const history = useHistory();
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const [editData, setEditData] = useState({
     type: 'username',
     title: '제가 뭐라고 부르면 좋을까요?',
@@ -27,7 +35,7 @@ const UserInformation = () => {
         });
       }
 
-      setIsModalOpen(true);
+      setIsEditModalOpen(true);
     },
     [myPageData.monsterName],
   );
@@ -35,48 +43,65 @@ const UserInformation = () => {
   const closeModal = useCallback(() => {
     // state 초기화
     setEditData({
+      type: 'username',
       title: '제가 뭐라고 부르면 좋을까요?',
       value: myPageData.username,
     });
-    setIsModalOpen(false);
+    setIsEditModalOpen(false);
   }, [myPageData.username]);
 
-  const handleChangeValue = (value) =>
-    setEditData((editData) => ({
-      ...editData,
-      value,
-    }));
+  const handleChangeValue = useCallback(
+    (value) => {
+      setEditData((editData) => ({
+        ...editData,
+        value,
+      }));
+    },
+    [editData.value],
+  );
+
+  const logoutUser = () => {
+    const token = getCookie('accessToken');
+
+    if (!token) {
+      <div>먼저 로그인을 해주세요!</div>;
+    }
+    window.localStorage.removeItem('habitAccess');
+    window.localStorage.removeItem('habitRefresh');
+    window.localStorage.removeItem('isFirstLogin');
+    window.localStorage.removeItem('isOnboarding');
+    resetAuth();
+    setIsLogoutModalOpen(false);
+    history.replace('/login', null);
+  };
 
   const userInfoList = [
     {
       title: '닉네임',
       contents: myPageData.username,
-      isPossibleEdit: true,
       handleClick: () => openModal('username'),
     },
     {
       title: '몬스터 이름',
       contents: myPageData.monsterName,
-      isPossibleEdit: true,
       handleClick: () => openModal('monsterName'),
     },
     {
       title: '몬스터 코드',
       contents: myPageData.monsterCode,
-      isPossibleEdit: false,
     },
     {
       title: '현재 버전',
       contents: 'V_1.0.0',
-      isPossibleEdit: false,
     },
     {
       title: '로그아웃',
       contents: '',
-      isPossibleEdit: false,
-      buttonType: true,
+      handleClick: () => setIsLogoutModalOpen(true),
+      isLogout: true,
     },
   ];
+
   return (
     <>
       <UserInfoList>
@@ -89,14 +114,28 @@ const UserInformation = () => {
           );
         })}
       </UserInfoList>
-      {isModalOpen && (
-        <Modal open={isModalOpen} onClose={closeModal}>
+      {isEditModalOpen && (
+        <Modal open={isEditModalOpen} onClose={closeModal}>
           <EditBox
             type={editData.type}
             editValue={editData.value}
             handleChangeValue={handleChangeValue}
             pageTitleText={editData.title}
             closeModal={closeModal}
+          />
+        </Modal>
+      )}
+      {isLogoutModalOpen && (
+        <Modal
+          open={isLogoutModalOpen}
+          onClose={() => setIsLogoutModalOpen(false)}
+          blurmode={true}
+        >
+          <BottomDialog
+            title="정말 로그아웃하시겠어요?"
+            activeButtonText="로그아웃하기"
+            onActive={() => logoutUser()}
+            onClose={() => setIsLogoutModalOpen(false)}
           />
         </Modal>
       )}
